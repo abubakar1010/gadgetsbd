8f51ebf2f7ec19cdac5e6691c1bd957eb51b7d35
abu bakar <abubakar850772@gmail.com>
2024-11-16 14:29:52 +0600

    validate login and register form with react hooks form

--- gadgetsbd_client/src/components/Auth/Login.tsx
+++ gadgetsbd_client/src/components/Auth/Login.tsx
@@ -1,8 +1,29 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+interface FormData {
+	name: string;
+	email: string;
+	password: string;
+	role: string;
+}
+
 const Login = () => {
+	const {
+		register,
+		// setValue,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>();
+
+	const onSubmit: SubmitHandler<FormData> = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<div>
 			<div className="hero bg-base-200 min-h-screen">
-				<div className="hero-content flex-col lg:flex-row-reverse">
+				<div className="hero-content flex-col lg:flex-row">
 					<div className="text-center lg:text-left">
 						<h1 className="text-5xl font-bold">Login now!</h1>
 						<p className="py-6">
@@ -12,7 +33,8 @@ const Login = () => {
 						</p>
 					</div>
 					<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-						<form className="card-body">
+						<form onSubmit={handleSubmit(onSubmit)} className="card-body">
+							
 							<div className="form-control">
 								<label className="label">
 									<span className="label-text">Email</span>
@@ -21,8 +43,11 @@ const Login = () => {
 									type="email"
 									placeholder="email"
 									className="input input-bordered"
-									required
+									{...register("email", { required: true })}
 								/>
+								{errors.email?.type === "required" && (
+									<p className={"text-red-500 pt-2"} >Email is required</p>
+								)}
 							</div>
 							<div className="form-control">
 								<label className="label">
@@ -32,16 +57,31 @@ const Login = () => {
 									type="password"
 									placeholder="password"
 									className="input input-bordered"
-									required
+									{...register("password", { required: true, minLength: 6, maxLength: 30 })}
 								/>
-								<label className="label">
-									<a href="#" className="label-text-alt link link-hover">
-										Forgot password?
-									</a>
-								</label>
+								{errors.password?.type === "required" && (
+									<p className={"text-red-500 pt-2"} >Password is required</p>
+								)}
+								{errors.password?.type === "minLength" && (
+									<p className={"text-red-500 pt-2"} >Password must be at least 6 character long</p>
+								)}
+								{errors.password?.type === "maxLength" && (
+									<p className={"text-red-500 pt-2"} >
+										Password is too long. Please limit it to 30 characters.
+									</p>
+								)}
 							</div>
+							
 							<div className="form-control mt-6">
-								<button className="btn btn-primary">Login</button>
+								<button type="submit" className="btn btn-primary">
+									Login
+								</button>
+								<div className=" flex justify-center items-center gap-2">
+								<span>Don't have an account?</span>
+								<Link className=" text-accent underline" to={"/register"}>
+								Register
+								</Link>
+								</div>
 							</div>
 						</form>
 					</div>
--- gadgetsbd_client/src/components/Auth/Register.tsx
+++ gadgetsbd_client/src/components/Auth/Register.tsx
@@ -1,8 +1,29 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+interface FormData {
+	name: string;
+	email: string;
+	password: string;
+	role: string;
+}
+
 const Register = () => {
+	const {
+		register,
+		// setValue,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>();
+
+	const onSubmit: SubmitHandler<FormData> = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<div>
 			<div className="hero bg-base-200 min-h-screen">
-				<div className="hero-content flex-col lg:flex-row-reverse">
+				<div className="hero-content flex-col lg:flex-row">
 					<div className="text-center lg:text-left">
 						<h1 className="text-5xl font-bold">Login now!</h1>
 						<p className="py-6">
@@ -12,7 +33,21 @@ const Register = () => {
 						</p>
 					</div>
 					<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-						<form className="card-body">
+						<form onSubmit={handleSubmit(onSubmit)} className="card-body">
+							<div className="form-control">
+								<label className="label">
+									<span className="label-text">Name</span>
+								</label>
+								<input
+									type="name"
+									placeholder="name"
+									className="input input-bordered"
+									{...register("name", { required: true })}
+								/>
+								{errors.name?.type === "required" && (
+									<p className={"text-red-500 pt-2"} >Name is required</p>
+								)}
+							</div>
 							<div className="form-control">
 								<label className="label">
 									<span className="label-text">Email</span>
@@ -21,8 +56,11 @@ const Register = () => {
 									type="email"
 									placeholder="email"
 									className="input input-bordered"
-									required
+									{...register("email", { required: true })}
 								/>
+								{errors.email?.type === "required" && (
+									<p className={"text-red-500 pt-2"} >Email is required</p>
+								)}
 							</div>
 							<div className="form-control">
 								<label className="label">
@@ -32,16 +70,46 @@ const Register = () => {
 									type="password"
 									placeholder="password"
 									className="input input-bordered"
-									required
+									{...register("password", { required: true, minLength: 6, maxLength: 30 })}
 								/>
+								{errors.password?.type === "required" && (
+									<p className={"text-red-500 pt-2"} >Password is required</p>
+								)}
+								{errors.password?.type === "minLength" && (
+									<p className={"text-red-500 pt-2"} >Password must be at least 6 character long</p>
+								)}
+								{errors.password?.type === "maxLength" && (
+									<p className={"text-red-500 pt-2"} >
+										Password is too long. Please limit it to 30 characters.
+									</p>
+								)}
+							</div>
+							<div className="form-control">
 								<label className="label">
-									<a href="#" className="label-text-alt link link-hover">
-										Forgot password?
-									</a>
+									<span className="label-text">Role</span>
 								</label>
+								<select
+									className="input input-bordered"
+									defaultValue={"Buyer"}
+									{...register("role", { required: true })}
+								>
+									<option value="Seller">Seller</option>
+									<option value="Buyer">Buyer</option>
+								</select>
+								{errors.role?.type === "required" && (
+									<p className={"text-red-500 pt-2"} >Role is required</p>
+								)}
 							</div>
 							<div className="form-control mt-6">
-								<button className="btn btn-primary">Login</button>
+								<button type="submit" className="btn btn-accent">
+									Register
+								</button>
+								<div className=" flex justify-center items-center gap-2">
+								<span>Already have an account?</span>
+								<Link className=" text-accent underline" to={"/login"}>
+								login
+								</Link>
+								</div>
 							</div>
 						</form>
 					</div>
