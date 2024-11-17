b833dee9f3a72e3a510c3d2c7a3fe2354871316c
abu bakar <abubakar850772@gmail.com>
2024-11-17 15:10:25 +0600

    added all product endpoints for fetched all product

--- gadgetsbd_server/index.js
+++ gadgetsbd_server/index.js
@@ -30,6 +30,45 @@ async function connectDD() {
 		);
 
 		const userCollection = client.db("gadgetsbd").collection("users");
+		const productCollection = client.db("gadgetsbd").collection("products");
+
+		//verify jwt toke
+
+		const verifyToken = async (req, res, next) => {
+			const data = req.headers.authorization;
+
+			const token = data?.split(" ")[1];
+
+			if (token) {
+				jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+					if (err) res.status(403).json("token is not valid");
+					req.user = user;
+					next();
+				});
+			} else {
+				return res.status(401).json("You are not authenticated");
+			}
+		};
+		const verifySeller = async (req, res, next) => {
+			const data = req.headers.authorization;
+
+			const token = data?.split(" ")[1];
+
+			if (token) {
+				jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
+					if (err) res.status(403).json("token is not valid");
+					const result = await userCollection.findOne({ email: user.email });
+					if (!result) res.status(404).json({ message: "user not found" });
+					if (result.email === "Seller") {
+						next();
+					}else{
+                        res.status(401).json({message: "unAuthorized user"})
+                    }
+				});
+			} else {
+				return res.status(401).json("You are not authenticated");
+			}
+		};
 
 		// custom middleware
 
@@ -44,44 +83,101 @@ async function connectDD() {
 			res.send({ token });
 		});
 
+		// user endpoints
 
-        // user endpoints 
+		// create a new user
 
-        app.post("/user", async(req, res) => {
-            const {user} = req.body;
+		app.post("/create-user", async (req, res) => {
+			const { user } = req.body;
 
-            console.log(req.body, user);
+			console.log(req.body, user);
+
+			const existedUser = await userCollection.findOne({ email: user.email });
+
+			if (existedUser) return res.json({ message: "User already exist" });
+
+			const result = await userCollection.insertOne(user);
+
+			res.json({ message: "user created successfully", result });
+		});
+
+		//get single user
+
+		app.get("/get-user", async (req, res) => {
+			const email = req.query.email;
+
+			const result = await userCollection.findOne({ email });
+			if (!result) return res.json({ message: "User not found" });
+
+			res.json({ message: "User fetched successfully", result });
+		});
+
+		// products endpoints
+
+		// insert a new products
+
+		app.post("/add-product", async (req, res) => {
+			const { product } = req.body;
             
 
-            const existedUser = await userCollection.findOne({ email: user.email })
+			const existedProduct = await productCollection.findOne({
+				title: product.title,
+			});
 
-            if(existedUser) return res.json({message: "User already exist"})
+			if (existedProduct) return res.json({ message: "Product already exist" });
 
-                const result = await userCollection.insertOne(user)
+			const result = await productCollection.insertOne(product);
 
-                res.send(result)
+			res.json({ message: "Product successfully added", result });
+		});
+
+		// get single product
+
+		app.get("/single-product/:name", async (req, res) => {
+			const name = req.params.name;
+
+			const result = await productCollection.findOne({ name });
+
+			if (!result) return res.json({ message: "Product not found" });
+
+			res.json({ message: "product fetched successfully", result });
+		});
+
+
+        app.get("/all-product", async(req, res) => {
+
+            const {title, category, brand, sort} = req.query;
+
+            const query = {};
+
+            if(title){
+                query.title = { $regex: title, $options: "i"}
+            }
+            if(category){
+                query.category = { $regex: category, $options: "i"}
+            }
+            if(brand){
+                query.brand = { $regex: brand, $options: "i"}
+            }
+            if(title){
+                query.sort = sort === "acc"? 1 : -1
+            }
+
+			const result = await productCollection.find(query).sort({price: sort}).toArray()
+			if(!result) res.status(404).json({message: "Product not found", result: null})
+				res.json({message: "successfully fetched products", result})
         })
 
+		app.get("/", (_req, res) => {
+			res.send("server connected");
+		});
 
-
-
-
-
-
-
-        app.get("/", (_req, res) => {
-            res.send("server connected");
-        });
-        
-        app.listen(port, () => {
-            console.log(`server connected on port ${port}`);
-        });
-
+		app.listen(port, () => {
+			console.log(`server connected on port ${port}`);
+		});
 	} catch (error) {
 		console.log("Mongodb connection failed", error);
 	}
 }
 
 connectDD();
-
-
