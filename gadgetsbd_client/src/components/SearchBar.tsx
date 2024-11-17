8a021f5d836783f1ef0070c804942caf750979ce
abu bakar <abubakar850772@gmail.com>
2024-11-17 23:19:55 +0600

    fix search functionalities issues

--- gadgetsbd_client/src/components/SearchBar.tsx
+++ gadgetsbd_client/src/components/SearchBar.tsx
@@ -1,19 +1,34 @@
-export default function SearchBar() {
+import { FC, FormEvent } from "react";
+
+interface SearchBarProps {
+	handleSearch: (event: FormEvent<HTMLFormElement>) => void;
+}
+const SearchBar: FC<SearchBarProps> = ({ handleSearch }) => {
 	return (
 		<>
-			<div className="form-control flex-row gap-2">
-				<div>
-                <input
-					type="text"
-					placeholder="Search"
-					className="input input-bordered w-24 md:w-auto lg:w-64"
-				/>
-                </div>
-                <div>
-                <button className=" btn btn-accent w-24">Search</button>
-
-                </div>
+			<div className="">
+				<form
+					action=""
+					className=" flex flex-row gap-4"
+					onSubmit={handleSearch}
+				>
+					<div>
+						<input
+							type="text"
+							name="search"
+							placeholder="Search"
+							className="input input-bordered w-24 md:w-auto lg:w-64"
+						/>
+					</div>
+					<div>
+						<button type="submit" className=" btn btn-accent w-24">
+							Search
+						</button>
+					</div>
+				</form>
 			</div>
 		</>
 	);
-}
+};
+
+export default SearchBar;
