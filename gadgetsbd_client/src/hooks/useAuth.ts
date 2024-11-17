80c8538d01d1656381c903743a6ff7da72e1fd6a
abu bakar <abubakar850772@gmail.com>
2024-11-17 15:08:36 +0600

    added custom hook for get user

--- gadgetsbd_client/src/hooks/useAuth.ts
+++ gadgetsbd_client/src/hooks/useAuth.ts
@@ -1,7 +1,9 @@
 import { useContext } from "react"
 import { AuthContext } from "../AuthProvider/AuthProvider"
+// import { CircleLoader } from "react-spinners";
 
 export const useAuth = () => {
     const auth = useContext(AuthContext)
+    // if(auth?.loading) return <CircleLoader/>
     return auth
 }
