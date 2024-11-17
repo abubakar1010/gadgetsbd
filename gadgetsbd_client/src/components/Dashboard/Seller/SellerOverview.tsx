46a5d1a866e35be40789fdb5849a2ff1f434f857
abu bakar <abubakar850772@gmail.com>
2024-11-18 02:21:38 +0600

    added buyer and seller overview routes

---
+++ gadgetsbd_client/src/components/Dashboard/Seller/SellerOverview.tsx
@@ -0,0 +1,10 @@
+
+const SellerOverview = () => {
+    return (
+        <div className="flex justify-center items-center text-2xl">
+            <h1>Seller Overview</h1>
+        </div>
+    );
+};
+
+export default SellerOverview;
