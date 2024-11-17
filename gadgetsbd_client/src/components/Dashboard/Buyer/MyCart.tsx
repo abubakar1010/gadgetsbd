005354495d026efd9dfbaa3999aeac4a2ffa5821
abu bakar <abubakar850772@gmail.com>
2024-11-18 02:19:45 +0600

    added my cart buyer route

---
+++ gadgetsbd_client/src/components/Dashboard/Buyer/MyCart.tsx
@@ -0,0 +1,12 @@
+
+const MyCart = () => {
+    return (
+        <>
+            <div className="flex justify-center items-center text-2xl">
+            <h1>My cart</h1>
+        </div>
+        </>
+    );
+};
+
+export default MyCart;
